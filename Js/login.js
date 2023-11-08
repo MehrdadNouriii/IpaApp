b82c88823f347400d857197735bf463687a5fdea
Mehrdad-Nouri <mehrdadnouri777518@gmail.com>
2023-11-08 20:26:08 +0330

    Nav&LoginChanged

--- Js/login.js
+++ Js/login.js
@@ -1,10 +1,10 @@
-const signUpButton = document.getElementById('signUp')
-const signInButton = document.getElementById('signIn')
-const container = document.getElementById('container')
+let container = document.getElementById('container')
 
-signUpButton.addEventListener('click', () => { 
-    container.classList.add("right-panel-active");
-})
-signInButton.addEventListener('click', () =>{
-    container.classList.remove("right-panel-active");
-})
+toggle = () => {
+    container.classList.toggle('sign-in')
+    container.classList.toggle('sign-up')
+}
+
+setTimeout(() => {
+    container.classList.add('sign-in')
+}, 200)
